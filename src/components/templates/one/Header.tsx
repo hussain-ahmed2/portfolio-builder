357dc73fe4330bfadca78a69f61f31d2d66f8e2e
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-25 17:55:02 +0600

    create temple one header

---
+++ src/components/templates/one/Header.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Header({
+	logo = "Portfolio",
+	navigation = [
+		{
+			name: "Home",
+			href: "/",
+		},
+		{
+			name: "About",
+			href: "#about",
+		},
+		{
+			name: "Portfolio",
+			href: "#portfolio",
+		},
+		{
+			name: "Contact",
+			href: "#contact",
+		},
+	],
+}: {
+	logo?: string;
+	navigation?: {
+		name: string;
+		href: string;
+	}[];
+}) {
+	const [open, setOpen] = useState<boolean>(false);
+
+	function handleToggle() {
+		setOpen((prev) => !prev);
+	}
+
+	function handleClose() {
+		if (window.innerWidth < 768) {
+			setOpen(false);
+		}
+	}
+
+	return (
+		<header className="border-b border-neutral-200 bg-white">
+			<nav className="flex min-h-16 items-center justify-between">
+				<Link
+					onClick={handleClose}
+					href="/"
+					className="px-8 font-extrabold text-2xl text-emerald-500"
+				>
+					{logo}
+				</Link>
+
+				<div className="max-md:absolute top-16 right-0 left-0 h-fit max-md:max-w-screen overflow-hidden">
+					<div
+						className={`flex flex-col left-0 max-md:w-full max-md:bg-neutral-50 md:gap-8 md:pe-8 md:flex-row transition duration-300 max-md:border max-md:border-neutral-200 ${
+							open
+								? "max-md:translate-x-0"
+								: "max-md:translate-x-full"
+						}`}
+					>
+						{navigation.map((item, index) => (
+							<Link
+								onClick={handleClose}
+								key={index}
+								href={`/${item.href.toLowerCase()}`}
+								className="nav-link"
+							>
+								{item.name}
+							</Link>
+						))}
+					</div>
+				</div>
+
+				<button
+					onClick={handleToggle}
+					className="md:hidden border h-12 w-12 border-neutral-200 rounded-md flex justify-center items-center me-2 hover:bg-neutral-100 group transition duration-300 active:scale-95"
+				>
+					<Menu
+						className="group-hover:stroke-emerald-500 transition duration-300"
+						size={32}
+					/>
+				</button>
+			</nav>
+		</header>
+	);
+}

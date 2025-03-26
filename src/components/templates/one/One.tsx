8b49c6e75c0df879321914a4c8129b47f6a4bfb2
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-26 21:05:27 +0600

    add about component

--- src/components/templates/one/One.tsx
+++ src/components/templates/one/One.tsx
@@ -1,7 +1,15 @@
+import About from "./About";
 import Header from "./Header";
+import Hero from "./Hero";
 
 export default function One() {
-	return <div className="min-h-screen w-full bg-neutral-100 text-neutral-950">
-        <Header />
-    </div>;
+	return (
+		<div className="w-full bg-neutral-100 text-neutral-950">
+			<Header />
+			<div className="pt-16 max-w-7xl mx-auto">
+				<Hero />
+                <About />
+			</div>
+		</div>
+	);
 }
